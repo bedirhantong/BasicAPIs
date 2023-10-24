91ffc6eca859800a72f0ba5423be30d33dd29127
Bedirhan Tong <bedirhantongdev@gmail.com>
2023-10-24 17:43:57 +0300

    Dependency Injection and Logging is applied

---
+++ ProductApp/Controllers/ProductsController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ProductApp.Models;
+
+namespace ProductApp.Controllers
+{
+    [Route("api/products")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        /*
+         - istekler atıldığında konsolda yapılan işlem hakkında loglama yapmak için
+         - readonly olduğu için ya constructorda yada tanımladığın gibi değer vermelisin 
+         */
+        private readonly ILogger<ProductsController> _logger;
+
+        public ProductsController(ILogger<ProductsController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult GetAllProducts() {
+            var products = new List<Product>() { 
+                new Product() { Id = 1, ProductName = "Computer"},
+                new Product() { Id = 2, ProductName = "Keyboard"},
+                new Product() { Id = 3, ProductName = "Mouse"},
+            };
+            _logger.LogInformation("GetAllProducts action has been called");
+            return Ok(products);
+        }
+
+    }
+}
---
+++ ProductApp/Models/Product.cs
@@ -0,0 +1,19 @@
+﻿namespace ProductApp.Models
+{
+    public class Product
+    {
+        public Product()
+        {
+        }
+
+        public Product(int Id, String ProductName)
+        {
+            this.Id = Id;
+            this.ProductName = ProductName;
+        }
+
+        public int Id { get; set; }
+
+        public String? ProductName { get; set; }
+    }
+}
