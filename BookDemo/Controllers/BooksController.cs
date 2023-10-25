4bf7977d975792a96cede4afa11da185fb31e9d9
Bedirhan Tong <bedirhantongdev@gmail.com>
2023-10-25 11:33:35 +0300

    HTTP GET request example

---
+++ BookDemo/Controllers/BooksController.cs
@@ -0,0 +1,79 @@
+﻿using BookDemo.Data;
+using BookDemo.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookDemo.Controllers
+{
+    [Route("api/books")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetAllBooks()
+        {
+            var books = ApplicationContext.Books;
+            return Ok(books);
+        }
+
+
+        [HttpGet("{id:int}")]
+        public IActionResult GetOneBook([FromRoute(Name = "id")]int id)
+        {
+            /*
+             LINQ kullandık
+            - SingleOrDefault eğer varsa kendisi yoksa null döndürüyor.
+             */
+            var book = ApplicationContext.
+                Books.Where(b => b.Id.Equals(id))
+                .SingleOrDefault();
+
+            if (book is null) {
+                return NotFound();
+            }
+
+            return Ok(book);
+        }
+
+        [HttpPost]
+        public IActionResult CreateBook([FromBody()] Book book)
+        {
+            try
+            {
+                if (book is null)
+                {
+                    return BadRequest(); // 400 
+                }
+
+                ApplicationContext.Books.Add(book);
+                return StatusCode(201, book);
+            }
+            catch (Exception ex)
+            {
+                 return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpPut("{int:id}")]
+        public IActionResult ChangeBook([FromRoute(Name="id")] int id,[FromBody]Book book)
+        {
+            try
+            {
+                if (book is null)
+                {
+                    return BadRequest(); // 400 
+                }
+
+                ApplicationContext.Books.
+                    Where(b => b.Equals(book).;
+                return StatusCode(201, book);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+
+    }
+}
